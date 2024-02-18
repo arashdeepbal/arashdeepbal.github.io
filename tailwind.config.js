941d6bc03698a370362acfe5bb9369a0d387a6c9
Arash Bal <arashdeepbal@gmail.com>
2024-02-18 16:24:35 +0700

    Update to Tailwind
    Removing all old code on bootstrap and upgrade to Tailwind styling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': '#171717',
+      'secondary': '#fafafa',
+      'tertiary': '#ffffff',
+      'secondary-hover': '#dddddd',
+    },
+    extend: {},
+  },
+}
+
